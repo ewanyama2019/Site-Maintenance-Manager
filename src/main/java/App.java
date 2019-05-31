baa1f11edd308e1654b32dc6f5c7068e2d916468
Eric Wanyama <wafulawanyama@gmail.com>
2019-05-31 13:38:00 +0300

    Added Index, site, engineer, success and faqs and created a reusable layout

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import static spark.Spark.*;
 
 public class App {
+    public static void main(String[] args) {
+
+
+    // ------Page routing (referring to handle bars ------- //
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/engineer", (request, response) -> {
+            return new ModelAndView(new HashMap(), "engineer.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/site", (request, response) -> {
+            return new ModelAndView(new HashMap(), "site.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/faqs", (request, response) -> {
+            return new ModelAndView(new HashMap(), "faqs.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/success", (request, response) -> {
+            return new ModelAndView(new HashMap(), "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+    }
 }
