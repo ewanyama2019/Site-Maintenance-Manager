9104f4c5d8a1d7d80fbc127066cfb684fbb0d209
Eric Wanyama <wafulawanyama@gmail.com>
2019-06-02 05:34:24 +0300

    Added a test in SiteTest.java to check if Site object has initialised correctly

--- src/test/java/models/SiteTest.java
+++ src/test/java/models/SiteTest.java
@@ -1,12 +1,18 @@
 package models;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.time.LocalDateTime;
-
+import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
+
 
 public class SiteTest {
+
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    @Test
+    public void site_instantiatesCorrectly_True() {
+        Site testSite = new Site("Sabasaba", 1);
+        assertEquals(true, testSite instanceof Site);
+    }
 }
